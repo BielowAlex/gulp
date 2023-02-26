10487898875c7c937ec5abd1e23d5a526a362a1c
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2023-02-26 19:45:02 +0200

    change lib gulp-gh-pages to gh-pages

--- gulp/tasks/git.js
+++ gulp/tasks/git.js
@@ -1,9 +1,8 @@
-import ghPages from 'gulp-gh-pages'
+import ghPages from 'gh-pages'
 
 export const git = () => {
-    return app.gulp.src(app.path.build.all)
-        .pipe(ghPages({
-            remoteUrl:'https://github.com/BielowAlex/repo',
-            branch:'master'
-        }))
+    return ghPages.publish(`${app.path.buildFolder}`, {
+        branch: 'master',
+        repo: `https://github.com/BielowAlex/${'repositoryName'}.git`,
+    })
 }
